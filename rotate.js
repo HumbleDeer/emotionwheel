015cc45632fb7c1c2ed8067e6b56c99b66500646
KiddieD <djsndcx@gmail.com>
2021-02-08 20:46:51 +0100

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,12 +1,13 @@
 /*jshint esversion: 6 */
 (function() {
     var rangeInput = document.getElementById("range-input");
+    var wheelInput;
     // Print the range value to the output
-    rangeInput.oninput = wheelUpdate;
+    rangeInput.oninput = wheelUpdate(rangeInput.value);
 
-    function wheelUpdate() {
-        document.getElementById("range-value").innerText = rangeInput.value; //Update the visual value
-        document.getElementById("wheel").style.transform = `rotate(${rangeInput.value}deg)`;
+    function wheelUpdate(wheelInput) {
+        document.getElementById("range-value").innerText = wheelInput; //Update the visual value
+        document.getElementById("wheel").style.transform = `rotate(${wheelInput}deg)`;
     }
     document.getElementById("lucky-button").addEventListener("click", function() {
         rangeInput = Math.random() * (360 - 0) - 180;
