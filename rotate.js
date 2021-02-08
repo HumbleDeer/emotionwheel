20d615e198aab3a67daa03f0ecda1fb47102acf5
KiddieD <djsndcx@gmail.com>
2021-02-08 20:25:36 +0100

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -2,15 +2,14 @@
 (function() {
     var rangeInput = document.getElementById("range-input");
     // Print the range value to the output
-    rangeInput.oninput = wheelUpdate;
+    rangeInput.oninput = wheelUpdate(rangeInput.value);
 
-    function wheelUpdate() {
+    function wheelUpdate(degrees) {
         document.getElementById("range-value").innerText = rangeInput.value; //Update the visual value
-        document.getElementById("wheel").style.transform = `rotate(${rangeInput.value}deg)`;
+        document.getElementById("wheel").style.transform = `rotate(${degrees}deg)`;
     }
     
     document.getElementById("feeling-lucky").addEventListener("click", function() {
-        rangeInput = Math.random() * (180 - (-180)) + (-180);
-        wheelUpdate();
+        wheelUpdate(Math.random() * (180 - (-180)) + (-180));
     });
 })();
