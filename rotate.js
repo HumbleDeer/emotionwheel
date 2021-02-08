65c51f59e55e91b4224965e810a8cc5783f74636
KiddieD <djsndcx@gmail.com>
2021-02-08 20:01:50 +0100

    joker

--- rotate.js
+++ rotate.js
@@ -1,13 +1,16 @@
+/*jshint esversion: 6 */
 (function() {
     var rangeInput = document.getElementById("range-input");
-    var rangeValue = document.getElementById("range-value");
-    var wheel = document.getElementById("wheel")
-// Print the range value to the output
+    // Print the range value to the output
     rangeInput.oninput = wheelUpdate;
 
     function wheelUpdate() {
-        rangeValue.innerText = rangeInput.value; //Update the visual value
-        wheel.style.transform = `rotate(${rangeInput.value}deg)`;
+        document.getElementById("range-value").innerText = rangeInput.value; //Update the visual value
+        document.getElementById("wheel").style.transform = `rotate(${rangeInput.value}deg)`;
     }
+    
+    document.getElementById("lucky-button").addEventListener("click", function() {
+        rangeInput = Math.random() * (max - min) + min;
+        wheelUpdate();
+    });
 })();
- 
